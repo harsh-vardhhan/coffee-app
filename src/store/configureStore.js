b7e10d0ba739ec4696c2ebaa821670a545af013a
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2017-08-19 13:19:46 +0530

    redux devtools extension added

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,12 @@
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
+import {composeWithDevTools} from 'redux-devtools-extension';
 
 export default function configureStore(initialState) {
     return createStore(
         rootReducer,
         initialState,
-        applyMiddleware(thunk)
+        composeWithDevTools(applyMiddleware(thunk))
     );
 }
