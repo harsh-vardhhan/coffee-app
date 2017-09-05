b82bbbb8b9897a32212b119a66038cdc15ffc768
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2017-09-05 16:26:26 +0100

    flow annotation for action

--- src/actions/coffee.js
+++ src/actions/coffee.js
@@ -1,29 +1,30 @@
+//@flow
 import coffeeApi from '../api/coffee';
 import {ADD_COFFEE, DELETE_COFFEE, EDIT_COFFEE, GET_COFFEES} from '../constants/coffee';
 import type {Coffee} from '../types/Coffee';
 import type {Coffees} from '../types/Coffees';
 
 export const getCoffees = () => (
-    async (dispatch) => {
+    async (dispatch: any) => {
         const coffees: Coffees = await coffeeApi.getCoffees();
         dispatch({type: GET_COFFEES, coffees});
     }
 );
 
 export const addCoffees = (coffee: Coffee) => (
-    (dispatch) => {
+    (dispatch: any) => {
         dispatch({type: ADD_COFFEE, coffee});
     }
 );
 
 export const deleteCoffees = (selectedCoffee: number) => (
-    (dispatch) => {
+    (dispatch: any) => {
         dispatch({type: DELETE_COFFEE, selectedCoffee});
     }
 );
 
 export const editCoffees = (coffee: Coffee, selectedCoffee: number) => (
-    (dispatch) => {
+    (dispatch: any) => {
         dispatch({type: EDIT_COFFEE, coffee, selectedCoffee});
     }
 );
