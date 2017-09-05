8ba256d7c4fd0ab181d866b90a7f13a225de1ee4
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2017-09-05 14:17:54 +0100

    migrate api call from fetch to axios

--- src/api/coffee.js
+++ src/api/coffee.js
@@ -1,18 +1,15 @@
 //@flow
+// $FlowFixMe
+import axios from 'axios';
 import type {Coffees} from '../types/Coffees';
 
 const coffeeUrl = 'http://www.mocky.io/v2/59958469110000b300cc4216';
-const onSuccess = (response) => (response.json()).then((coffees: Coffees) => coffees);
+const onSuccess = ({data}: {data: Coffees}) => data;
 const onError = () => [];
 
 class coffeeApi {
     static getCoffees() {
-        return fetch(coffeeUrl, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then(onSuccess, onError);
+        return axios.get(coffeeUrl).then(onSuccess).catch(onError);
     }
 }
 
