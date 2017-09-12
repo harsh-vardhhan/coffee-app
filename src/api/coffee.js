ab55b29dd164e18562d5c077f5f041886dbfcf44
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2017-09-13 00:11:11 +0100

    saperate mock data

--- src/api/coffee.js
+++ src/api/coffee.js
@@ -1,25 +1,8 @@
 //@flow
 import axios from 'axios';
 import type {Coffees} from '../types/Coffees';
-
+import {coffeeMock} from '../mock/coffee.js';
 const coffeeUrl = 'http://www.mocky.io/v2/59958469110000b300cc4216';
-const coffeeMock = [
-    {
-        id: 1,
-        name: 'cappuccino',
-        price: 3.45
-    },
-    {
-        id: 2,
-        name: 'lattee',
-        price: 2.75
-    },
-    {
-        id: 3,
-        name: 'mocha',
-        price: 4.25
-    }
-];
 
 class coffeeApi {
     static getCoffees() {
---
+++ src/mock/coffee.js
@@ -0,0 +1,17 @@
+export const coffeeMock = [
+    {
+        id: 1,
+        name: 'cappuccino',
+        price: 3.45
+    },
+    {
+        id: 2,
+        name: 'lattee',
+        price: 2.75
+    },
+    {
+        id: 3,
+        name: 'mocha',
+        price: 4.25
+    }
+];
