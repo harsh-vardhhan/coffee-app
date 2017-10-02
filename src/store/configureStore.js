270d1a469e830ef32cc2b9d4f0013d22bbe2bd36
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2017-10-02 23:59:14 +0100

    store type definitions

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,8 +2,10 @@ import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 import {composeWithDevTools} from 'redux-devtools-extension';
+import type {Store} from '../types/Store';
+import type {State} from '../types/State';
 
-const configureStore = (initialState) => (
+const configureStore = (initialState: State): Store => (
     createStore(
         rootReducer,
         initialState,
