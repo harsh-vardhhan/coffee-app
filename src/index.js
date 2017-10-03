80c34a2c308c1fa058f57b1c6edaf544bcb503c5
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2017-10-03 17:02:31 +0100

    index.js flow typed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
+//@flow
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+//$FlowFixMe
 import './../node_modules/grommet/grommet-hpe.min.css';
 import App from './App';
 import {Provider} from 'react-redux';
 import configureStore from './store/configureStore';
+
+//$FlowFixMe
 const store = configureStore();
 
 ReactDOM.render(
+
+    //$FlowFixMe
     <Provider store={store}>
         <App/>
     </Provider>
