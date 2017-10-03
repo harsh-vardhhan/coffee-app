df7ff3d6d1fa39a43b3d83cbb3e0768af0332f5e
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2017-10-03 16:40:30 +0100

    configure store flow typed

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,17 @@
-import {createStore, applyMiddleware} from 'redux';
+//@flow
+import {createStore, applyMiddleware, type Store} from 'redux';
+
+//$FlowFixMe
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
+
+//$FlowFixMe
 import {composeWithDevTools} from 'redux-devtools-extension';
-import type {Store} from '../types/Store';
 import type {State} from '../types/State';
 
-const configureStore = (initialState: State): Store => (
+const configureStore = (initialState: State): Store<*> => (
+
+    //$FlowFixMe
     createStore(
         rootReducer,
         initialState,
