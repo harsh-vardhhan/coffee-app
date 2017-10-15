10b8a723adb548e4a893f4f71c48967f9467489b
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2017-10-15 01:56:04 +0100

    Format type Defs for Dispatch

--- src/types/Dispatch.js
+++ src/types/Dispatch.js
@@ -3,7 +3,23 @@ import type {Dispatch} from 'redux';
 import type {Coffee} from '../types/Coffee';
 import type {Coffees} from '../types/Coffees';
 
-export type DispatchGetCoffees = Dispatch<{type: string, coffees: Coffees}>;
-export type DispatchAddCoffees = Dispatch<{type: string, coffee: Coffee}>;
-export type DispatchDeleteCoffees = Dispatch<{type: string, selectedCoffee: number}>;
-export type DispatchEditCoffees = Dispatch<{type: string, coffee: Coffee, selectedCoffee: number}>;
+export type DispatchGetCoffees = Dispatch<{
+  type: string,
+  coffees: Coffees
+}>;
+
+export type DispatchAddCoffees = Dispatch<{
+  type: string,
+  coffee: Coffee
+}>;
+
+export type DispatchDeleteCoffees = Dispatch<{
+  type: string,
+  selectedCoffee: number
+}>;
+
+export type DispatchEditCoffees = Dispatch<{
+  type: string,
+  coffee: Coffee,
+  selectedCoffee: number
+}>;
